3a442f9a6ea8ddc392bbbd388d172a5ec3456817
2042Thrid <18604713262@163.com>
2020-07-15 16:08:20 -0400

    saved by Mike Yang

--- p1.py
+++ p1.py
@@ -1,13 +1,14 @@
 import sys
 from algorithms import *
-
-def drand48(seed):
+def drand48():
+  global seed
   a = 273673163155
   c = 138
   m = 281474976710656
+  n = 4294967296
   seed = (a * seed + c) % m
-  #print(seed)
-  return seed
+  x = seed >> 16
+  return float (x / n)
 
 
 
@@ -15,7 +16,11 @@ def drand48(seed):
 # n: the number of process generates (1<=n<=26)
 # using the drand() to identify the number of bursts time; using the exp-random to identify the cpu and I/O burst time
 def processGen(n):
+  processlist = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
   return 0
+
 #handle the ties in the order: CPU burst completion, I/O, new proces
 '''
 requires: a and b are not null
@@ -29,16 +34,19 @@ def handleTies(a , b):
   else:
     return b
 
-
 if __name__ == '__main__':
-  #get all the cmd/parameters
-  # n = int(sys.argv[1])
-  # seed = int(sys.argv[2])
-  # Lambda = float(sys.argv[3])
-  # upperbound = int(sys.argv[4])
-  # t_cs  = int(sys.argv[5])
-  # alpha = float(sys.argv[6])
-  # t_slice = float(sys.argv[7])
-  # rr_add = int(sys.argv[8])
-  print("test", drand48(12) % 10)
-  print()
+  # get all the cmd/parameters
+
+  '''
+    n = int(sys.argv[1])
+    seed = int(sys.argv[2])
+    Lambda = float(sys.argv[3])
+    upperbound = int(sys.argv[4])
+    t_cs  = int(sys.argv[5])
+    alpha = float(sys.argv[6])
+    t_slice = float(sys.argv[7])
+   rr_add = int(sys.argv[8]
+  '''
+  seed = 1000
+  for i in range(10):
+    print("test", drand48())
