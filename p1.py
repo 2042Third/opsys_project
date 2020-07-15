b2e2d74e757c38d9be348f14d33788da169b47f7
Jay <Jay>
2020-07-15 17:07:07 +0800

    Jieyang

--- p1.py
+++ p1.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def drand48(seed):
   a = 273673163155
   c = 138
@@ -9,16 +8,23 @@ def drand48(seed):
   print(seed)
   return seed
 
+
+
 # generate the processes
-def processGen():
+# n: the number of process generates (1<=n<=26)
+# using the drand() to identify the number of bursts time; using the exp-random to identify the cpu and I/O burst time
+def processGen(n):
+  return 1
 
 
 
 if __name__ == '__main__':
-  print("This is OPSYS project")
-  drand48(1000)
-  print("xuchang")
-  print("Hello!")
-  print("test")
-  print(sys.argv)
+# get all the cmd/parameters
+  n = int(sys.argv[1])
+  seed = int(sys.argv[2])
+  Lambda = float(sys.argv[3])
+  upperbound = int(sys.argv[4])
+  t_cs  = int(sys.argv[5])
+  alpha = float(sys.argv[6])
+
   print("test", drand48(12) % 10)
