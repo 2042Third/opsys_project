683748fad76eb3a565dfe2da4b21c84db60a7d0d
B14ck0ut <gavinhe00@gmail.com>
2020-07-14 22:41:51 -0700

    fix

--- p1.py
+++ p1.py
@@ -1,7 +1,19 @@
 import sys
 
+
+def drand48(seed):
+  a = 273673163155
+  c = 138
+  m = 281474976710656
+  seed = (a * seed + c) % m
+  print(seed)
+  # return ??? <- find what to return
+
+
+
 if __name__ == '__main__':
   print("This is OPSYS project")
+  drand48(1000)
   print("xuchang")
   print("Hello!")
   print(sys.argv)
