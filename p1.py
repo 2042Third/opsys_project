581df804247e59c549c485f5299c5d2777377d02
2042Thrid <18604713262@163.com>
2020-07-15 05:50:43 -0400

    saved by Mike Yang

--- p1.py
+++ p1.py
@@ -7,33 +7,9 @@ def drand48(seed):
   m = 281474976710656
   seed = (a * seed + c) % m
   print(seed)
-  # return ??? <- find what to return
+  return seed
 
-  # class Rand48(object):
-  #   def __init__(self, seed):
-  #     self.n = seed
-  #
-  #   def seed(self, seed):
-  #     self.n = seed
-  #
-  #   def srand(self, seed):
-  #     self.n = (seed << 16) + 0x330e
-  #
-  #   def next(self):
-  #     self.n = (25214903917 * self.n + 11) & (2 ** 48 - 1)
-  #     return self.n
-  #
-  #   def drand(self):
-  #     return self.next() / 2 ** 48
-  #
-  #   def lrand(self):
-  #     return self.next() >> 17
-  #
-  #   def mrand(self):
-  #     n = self.next() >> 16
-  #     if n & (1 << 31):
-  #       n -= 1 << 32
-  #     return n
+
 
 
 if __name__ == '__main__':
@@ -43,3 +19,4 @@ if __name__ == '__main__':
   print("Hello!")
   print("test")
   print(sys.argv)
+  print("test",drand48(12) % 10)
