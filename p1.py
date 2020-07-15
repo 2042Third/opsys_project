3abcd4059a3b83e76c01ff47076fc42436abf6ae
2042Thrid <18604713262@163.com>
2020-07-15 16:02:30 -0400

    saved by Mike Yang

--- p1.py
+++ p1.py
@@ -17,18 +17,28 @@ def drand48(seed):
 def processGen(n):
   return 0
 #handle the ties in the order: CPU burst completion, I/O, new proces
-def handleTies():
-  return 0
+'''
+requires: a and b are not null
+params: char a , char b; process id's 
+effects: none
+returns: char t, the smaller name character
+'''
+def handleTies(a , b):
+  if(a < b):
+    return a
+  else:
+    return b
+
 
 if __name__ == '__main__':
   #get all the cmd/parameters
-  n = int(sys.argv[1])
-  seed = int(sys.argv[2])
-  Lambda = float(sys.argv[3])
-  upperbound = int(sys.argv[4])
-  t_cs  = int(sys.argv[5])
-  alpha = float(sys.argv[6])
-  t_slice = float(sys.argv[7])
-  rr_add = int(sys.argv[8])
+  # n = int(sys.argv[1])
+  # seed = int(sys.argv[2])
+  # Lambda = float(sys.argv[3])
+  # upperbound = int(sys.argv[4])
+  # t_cs  = int(sys.argv[5])
+  # alpha = float(sys.argv[6])
+  # t_slice = float(sys.argv[7])
+  # rr_add = int(sys.argv[8])
   print("test", drand48(12) % 10)
-  print(n)
+  print()
