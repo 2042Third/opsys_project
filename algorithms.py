9d53e7c8c78ffe477ca83d805ea2575d6d021af7
2042Thrid <18604713262@163.com>
2020-07-15 17:40:01 -0400

    saved

--- algorithms.py
+++ algorithms.py
@@ -7,8 +7,9 @@ returns: none
 Note: Simulates first-come-first-serve modal in CPU scheduling. 
         Assuming data[0] is process 'A', and data[1] is process 'B', and so on.
 '''
-def FCFS(data):
 
+
+def FCFS(data):
     return 0
 
 
@@ -20,5 +21,23 @@ def SRT():
     return 0
 
 
-def RR():
+'''
+param: list data, t, bne, data = [{arrival: t, cpuburst: t, ioburst: t},{}...],
+ t = time slice, bne = beginning or end of the sequence
+effects: none
+returns: none
+Note: Simulates Round-Robin modal in CPU scheduling. 
+        Assuming data[0] is process 'A', and data[1] is process 'B', and so on.
+        If 0 is given for bne, the default is END.
+'''
+
+def RR(data, time, bne):
+    if bne == 0:
+        bne = True
+    else:
+        if bne == "BEGINNING":
+            bne = False
+        else:
+            bne = True
+
     return 0
